50d3bfcdd07a7069536533e78c415d0e4740a86e
Jhvonsky <70561984+signofcommunication@users.noreply.github.com>
2024-03-20 19:38:15 +0700

    Update read_single.h

--- Struktur Data/read_single.h
+++ Struktur Data/read_single.h
@@ -1,34 +1,34 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+using namespace std;
 
 // Fungsi untuk membaca data tunggal berdasarkan nomor baris
-std::string readDataByLineNumber(const std::string& filename, int lineNumber) {
-    std::ifstream file(filename);
+string readDataByLineNumber(const string& filename, int lineNumber) {
+    ifstream file(filename);
     if (file.is_open()) {
-        std::string line;
+        string line;
         int currentLineNumber = 0;
-        while (std::getline(file, line)) {
+        while (getline(file, line)) {
             currentLineNumber++;
             if (currentLineNumber == lineNumber) {
                 return line;
             }
         }
         // Jika nomor baris tidak ditemukan
-        std::cerr << "Nomor baris tidak ditemukan." << std::endl;
+        cerr << "Nomor baris tidak ditemukan." << endl;
         file.close();
     } else {
-        std::cerr << "Gagal membuka file: " << filename << std::endl;
+        cerr << "Gagal membuka file: " << filename << endl;
     }
     return ""; // Return empty string jika tidak ada data yang ditemukan
 }
 
 void read_single(int lineNumber) {
-    std::string filename = "database.txt";
+    string filename = "database.txt";
 
-    std::string data = readDataByLineNumber(filename, lineNumber);
+    string data = readDataByLineNumber(filename, lineNumber);
     if (!data.empty()) {
-        std::cout << "Data pada baris " << lineNumber << ": " << data << std::endl;
+        cout << "Data pada baris " << lineNumber << ": " << data << endl;
     }
 }
-
