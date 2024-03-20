961110f143450ef449c482005050c9cbcb86a55f
Jhvonsky <70561984+signofcommunication@users.noreply.github.com>
2024-03-20 19:37:58 +0700

    Update delete.h

--- Struktur Data/delete.h
+++ Struktur Data/delete.h
@@ -2,52 +2,52 @@
 #include <fstream>
 #include <sstream>
 #include <string>
+using namespace std;
 
 // Fungsi untuk membaca keseluruhan file ke dalam string
-std::string bacaFiles(const std::string& filename) {
-    std::ifstream file(filename);
+string bacaFiles(const string& filename) {
+    ifstream file(filename);
     if (file.is_open()) {
-        std::string content((std::istreambuf_iterator<char>(file)), std::istreambuf_iterator<char>());
+        string content((istreambuf_iterator<char>(file)), istreambuf_iterator<char>());
         file.close();
         return content;
     } else {
-        std::cerr << "Gagal membuka file: " << filename << std::endl;
+        cerr << "Gagal membuka file: " << filename << endl;
         return "";
     }
 }
 
 // Fungsi untuk menulis string ke dalam file
-void tulisFile(const std::string& content, const std::string& filename) {
-    std::ofstream file(filename);
+void tulisFile(const string& content, const string& filename) {
+    ofstream file(filename);
     if (file.is_open()) {
         file << content;
         file.close();
-        std::cout << "Data berhasil disimpan ke dalam file: " << filename << std::endl;
+        cout << "Data berhasil disimpan ke dalam file: " << filename << endl;
     } else {
-        std::cerr << "Gagal membuka file: " << filename << std::endl;
+        cerr << "Gagal membuka file: " << filename << endl;
     }
 }
 
 // Fungsi untuk menghapus data berdasarkan nomor baris
-void deleteDataByLineNumber(const std::string& filename, int lineNumber) {
-    std::string content = bacaFiles(filename);
+void deleteDataByLineNumber(const string& filename, int lineNumber) {
+    string content = bacaFiles(filename);
     if (!content.empty()) {
-        std::stringstream ss(content);
-        std::string newContent;
-        std::string line;
+        stringstream ss(content);
+        string newContent;
+        string line;
         int currentLineNumber = 1;
-        while (std::getline(ss, line)) {
+        while (getline(ss, line)) {
             if (currentLineNumber != lineNumber) {
                 newContent += line + "\n";
             }
             currentLineNumber++;
         }
         tulisFile(newContent, filename);
-        std::cout << "Baris " << lineNumber << " berhasil dihapus." << std::endl;
+        cout << "Baris " << lineNumber << " berhasil dihapus." << endl;
     }
 }
 
 void deleteData(int lineNumber) {
     deleteDataByLineNumber("database.txt", lineNumber);
 }
-
